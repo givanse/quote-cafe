6d49bed5125e251787af3b0d20d99c378a3effae
Gastón I. Silva <givanse@gmail.com>
2016-12-30 11:06:29 -0600

    add load button

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+# Quote Cafe
+
+[![Build Status](https://travis-ci.org/givanse/quote-cafe.svg?branch=master)](https://travis-ci.org/givanse/quote-cafe)
+
+http://quote.cafe
 
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
--- src/App.js
+++ src/App.js
@@ -3,15 +3,53 @@ import Quote from './quote.js';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: {
+        imgSrc: 'http://placehold.it/200x200',
+        author: 'Jane Doe',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        year: '1984'
+      }
+    };
+  }
+
+  renderQuote() {
+    return (
+      <Quote
+        imgSrc={this.state.quote.imgSrc}
+        author={this.state.quote.author} 
+        text={this.state.quote.text}
+        year={this.state.quote.year}
+      />
+    );
+  }
+
+  getNewQuote() {
+    let quote = {
+      imgSrc: 'img/feynman.jpg',
+      author: 'Cyril DeGrasse Tyson',
+      text: 'Never stop testing, and your advertising will never stop improving.',
+      year: '1849'
+    };
+    return quote;
+  }
+
+  loadNewQuote() {
+    let quote = this.getNewQuote();
+    this.setState({quote: quote});
+  }
+
   render() {
     return (
       <div className="App">
-        <Quote
-          imgSrc='img/feynman.jpg'
-          author='Cyril DeGrasse Tyson'
-          text='Never stop testing, and your advertising will never stop improving.'
-          year='1849'
-        />
+        {this.renderQuote()}
+
+        <button onClick={() => this.loadNewQuote()}>
+          reload
+        </button>
 
         <footer className="App-footer">
           <h1>quote cafe</h1>
